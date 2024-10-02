47b5c131240a3926e185d76894cc0f55080e6121
SheridanQ <xqi10@hawk.iit.edu>
2024-10-02 11:39:51 -0400

    clean data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and save the results in Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
 import wandb
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -18,39 +19,84 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    logger.info(f"Downloading artifact {args.input_artifact}")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv(artifact_local_path)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info(
+        f"Limit min and max price to {args.min_price} and {args.max_price} to remove outliers")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+
+    logger.info("Convert last_review to datetime")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    logger.info("Limit longitude and latitude")
+    idx = df['longitude'].between(-74.25, -
+                                  73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    logger.info("Save processed data to file.")
+    filename = "clean_sample.csv"
+    df.to_csv(filename, index=False)
+
+    logger.info("Output artifact")
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This steps cleans the data")
-
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact",
+        type=str,  # INSERT TYPE HERE: str, float or int,
+        help="Input artifact name",  # INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact",
+        type=str,  # INSERT TYPE HERE: str, float or int,
+        help="Output artifact name",  # INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_type",
+        type=str,  # INSERT TYPE HERE: str, float or int,
+        help="Output type",  # INSERT DESCRIPTION HERE,
         required=True
     )
 
+    parser.add_argument(
+        "--output_description",
+        type=str,  # INSERT TYPE HERE: str, float or int,
+        help="Output description",  # INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=float,  # INSERT TYPE HERE: str, float or int,
+        help="Minimum price",  # INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=float,  # INSERT TYPE HERE: str, float or int,
+        help="Maximum price",  # INSERT DESCRIPTION HERE,
+        required=True
+    )
 
     args = parser.parse_args()
 
